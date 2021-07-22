832a2b25c850860391c983cb21fef2efead6d083
anshul <mangalanshul1998@gmail.com>
2021-07-22 13:33:02 +0530

    Git Check

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 const static_path = path.join(__dirname, "../public");
 const template_path = path.join(__dirname, "../templates/views");
 const partials_path = path.join(__dirname, "../templates/partials");
-
+//Git check
 app.set('view engine', 'hbs');
 app.set('views', template_path);
 hbs.registerPartials(partials_path);
